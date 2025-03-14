891b1c97660ed00d49878bf30be589fcf349fea4
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2025-03-14 21:13:54 +0100

    feat: add initial image assets and utility functions

--- anchor/programs/prompthubsol/src/lib.rs
+++ anchor/programs/prompthubsol/src/lib.rs
@@ -2,7 +2,7 @@
 
 use anchor_lang::prelude::*;
 
-declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
+declare_id!("5E4bXyfwYPGEN1HmWKBM2yFQsJqD4rqFb7Dciund8ZVW");
 
 #[program]
 pub mod prompthubsol {
